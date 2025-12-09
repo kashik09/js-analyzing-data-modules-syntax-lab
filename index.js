22ab23bfea0e1462dad337e7e9779fa300ec5a65
kashik09 <kashiku789@gmail.com>
2025-12-09 23:13:20 +0300

    feat: add merge_date with current date in M/d/yyyy format

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function combineUsers(...args) {
     for (let i = 0; i < args.length; i++) {
         combinedObject.users = [...combinedObject.users, ...args[i]];
     }
+    
+    // Add today's date in M/d/yyyy format
+    combinedObject.merge_date = new Date().toString('M/d/yyyy');
 }
 
 module.exports = {
